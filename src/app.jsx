698ceee885bfe564cdfa44e10ddec41923ba2230
ycuevas <ycuevas@isf.do>
2023-05-19 14:30:51 -0400

    first commit

---
+++ src/app.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'preact/hooks'
+import preactLogo from './assets/preact.svg'
+import viteLogo from '/vite.svg'
+
+
+export function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+   <div>
+    klk
+   </div>
+    </>
+  )
+}
