7dc6edb4c54eae0a57122dca0c8ad0c789aefeca
ycuevas <ycuevas@isf.do>
2023-07-11 15:11:07 -0400

    autocomplete

--- src/components/login/login.jsx
+++ src/components/login/login.jsx
@@ -40,7 +40,7 @@ const Login = () => {
           </div>
           <div className="mb-3">
             <label for="exampleInputPassword1" className="form-label">Password</label>
-            <input type="password" className="form-control" id="exampleInputPassword1" value={password}
+            <input type="password" autoComplete='false' className="form-control" id="exampleInputPassword1" value={password}
               name={'password'}
               onChange={handleInput('password')} />
           </div>
